7f3678ea949af31f1519e243e558a9c531aecfc8
Chris Cornwell <chris@citizennet.com>
2016-04-04 15:39:20 -0700

    fix bug in getSchema call

--- src/cn-flex-form.service.js
+++ src/cn-flex-form.service.js
@@ -384,14 +384,16 @@
 
       //console.log('key:', key);
 
-      var first, matchArray;
+      var first, next, matchArray;
 
       while(key.length > 1) {
         first = key[0];
-        matchArray = first.match(/\[\d*]$/);
+        next = key[1];
+        matchArray = next.match(/^\d*$/);
         //if(first.slice(first.length - 2) === '[]') {
         if(matchArray) {
-          depth = depth[key.shift().slice(0, first.length - matchArray[0].length)].items.properties;
+          depth = depth[key.shift()].items.properties;
+          key.shift();
         }
         else {
           depth = depth[key.shift()].properties;
@@ -399,10 +401,10 @@
       }
 
       first = key[0];
-      matchArray = first.match(/\[\d*]$/);
+      matchArray = first.match(/^\d*$/);
 
       if(matchArray) {
-        return depth[first.slice(0, first.length - matchArray[0].length)].items;
+        return depth.items;
       }
 
       return depth[first];
