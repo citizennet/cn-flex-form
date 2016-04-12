969e28c6acd07bd6ee4f26be576beada06fbadd7
David <dzuch@citizennet.com>
2016-04-11 17:11:46 -0700

    updated toggle switch to use ng-model

--- src/schema-form-extensions.js
+++ src/schema-form-extensions.js
@@ -46,7 +46,6 @@
               ng-show="form.key"\
               ng-model-options="form.ngModelOptions"\
               ng-model="$$value$$"\
-              enabled="$$value$$"\
               sf-changed="form"\
               schema-validate="form"\
               on-value="form.onValue"\
