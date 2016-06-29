a6f1b56e42b4fd4ee3564ef62c1dec560ecb4855
David <dzuch@citizennet.com>
2016-06-28 17:06:13 -0700

    tiny cleanup

--- src/cn-flex-form.service.js
+++ src/cn-flex-form.service.js
@@ -29,7 +29,7 @@
   function cnFlexFormServiceProvider(schemaFormDecoratorsProvider,
                                      cnFlexFormTypesProvider) {
     return {
-      registerField: registerField,
+      registerField,
       $get: CNFlexFormService
     };
 
