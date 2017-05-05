10612b98dbf50734e2b4f691afdb110edac703c3
Chris Cornwell <chris@citizennet.com>
2017-05-05 14:38:02 -0700

    FBCM-1310 add back logic for removing readonly in processField

--- src/cn-flex-form.service.js
+++ src/cn-flex-form.service.js
@@ -397,6 +397,7 @@ function CNFlexFormService(
       if(schema) {
         field.schema = schema;
         if(schema.description) field.description = schema.description;
+        if(field.readonly && !schema.readonly) field.readonly = false;
         if(schema.type === 'array' && !('showClearAll' in field)) field.showClearAll = true;
       }
 
