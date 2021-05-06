1500751f2e7050cefe48ee7eb1db8426ad6e2f42
Jeremy Octeau <61431714+jocteau@users.noreply.github.com>
2021-05-06 11:00:30 +0200

    Fix select display batch form (#55)

--- src/cn-flex-form.service.js
+++ src/cn-flex-form.service.js
@@ -475,7 +475,6 @@ function CNFlexFormService(
 
     key = ObjectPath.parse(service.getKey(key));
     depth = depth || service.schema.schema.properties;
-
     let first, next;
 
     while(key.length > 1) {
@@ -1704,7 +1703,18 @@ function CNFlexFormService(
   function processSelectDisplay(selectDisplay) {
     const service = this;
     const schema = service.getSchema(selectDisplay.key);
-    const selectField = _.find(selectDisplay.items, 'selectField');
+    // Needed for batchform to check recursively
+    let selectField = null;
+    for (let item of selectDisplay.items) {
+      if (item.selectField) {
+        selectField = item;
+      } else if (item.items) {
+        selectField = _.find(item.items, 'selectField');
+      }
+      if (selectField) {
+        break;
+      }
+    }
 
     return schema && schema.type === 'array' ?
       service.setupArraySelectDisplay(selectDisplay, selectField) :
@@ -1783,7 +1793,12 @@ function CNFlexFormService(
     const service = this;
     const selectFieldKey = service.getKey(selectField.key);
 
-    _.each(selectDisplay.items, item => {
+    let selectDisplayItems = _(selectDisplay.items)
+        .map(item => item.items || [item])
+        .flatten()
+        .value();
+
+    _.each(selectDisplayItems, item => {
       if(item.selectField === true) return;
 
       const key = _.isArray(item.key) ? item.key : ObjectPath.parse(item.key);
@@ -1809,7 +1824,7 @@ function CNFlexFormService(
     var selectKey = service.getKey(selectField.key);
     var selectModel = service.parseExpression(selectKey, service.model);
     var selectValue = selectModel.get();
-    _.each(selectDisplay.items, function(item) {
+    _.each(selectDisplayItems, function(item) {
       var key = service.getKey(item.key);
       if(selectKey === key) return;
       var splitKey = ObjectPath.parse(key);
