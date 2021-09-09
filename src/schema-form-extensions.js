1f8b1b418399a86f752428945e714ce1207c2e48
Jo Jordan <joanne.k.m.jordan@gmail.com>
2021-09-09 03:29:49 -0700

    ensure exact matches for custom/lookalike audiences

--- src/schema-form-extensions.js
+++ src/schema-form-extensions.js
@@ -131,6 +131,7 @@ function addTemplates($templateCache) {
           allow-bulk="{{form.bulkAdd}}"
           bulk-delimiter="{{form.bulkDelimiter}}"
           bulk-placeholder="{{form.bulkPlaceholder}}"
+          bulk-add-exact="{{form.bulkAddExact}}"
           show-clear-all="{{form.showClearAll}}"
           show-clear-cache="{{form.showClearCache}}"
           show-button="true">
